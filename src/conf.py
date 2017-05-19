333ef19ae9182fd3ef2b4a98c8659e61ce6aec72
Ryan J. Kinnear <Ryan@Kinnear.ca>
2017-05-19 16:10:42 -0400

    skip grams and more sensible feature extraction pipeline

---
+++ src/conf.py
@@ -0,0 +1,41 @@
+import os
+cwd = os.getcwd()
+
+# Directories for importing on ipyparallel
+FEATURES_DIR = cwd + '/src/features'
+
+# Data file information
+RAW_DATA_DIR = cwd + '/data/raw/'
+RAW_DATA_FILES = ['test', 'train']
+TRAIN_FILE = 'train'
+RAW_DATA_EXT = '.csv'
+INTERIM_HDF_PATH = cwd + '/data/interim/interim_data.hdf'
+INTERIM_DATA_DIR = cwd + '/data/interim/'
+PROCESSED_DATA_DIR = cwd + '/data/processed/'
+
+# The character set used to filter all of the questions
+valid_chars = set(
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ?.!:;, ')
+
+# s3://rjtk/...
+S3_BUCKET = 'rjtk'
+S3_DATA_PATH = 'quora_question_pairs/dara'
+
+# Number of rows of data read at a time
+CHUNKSIZE = 2**14  # Chunk size is split between each ipyparallel worker
+TEST_ROWS = 16
+MASI_DISTANCE = 'masi_dist'
+EDIT_DISTANCE = 'edit_dist'
+JACCARD_DISTANCE = 'jaccard_dist'
+Q = ['question1', 'question2']
+Q_WORD_TOKENIZED = ['question1_word_tokenized',
+                    'question2_word_tokenized']
+Q_TAGGED = ['question1_pos_tagged',
+            'question2_pos_tagged']
+Q_TYPE = ['question1_type1',
+          'question2_type2']
+
+# tfidf/nmf params
+TFIDF_FEATURES = 20000
+N_NMF_COMPONENTS = 75
+SKIPGRAM_LIST = [(1, 0), (2, 3), (3, 2)]
---
+++ src/features/tfidf_features.py
@@ -0,0 +1,49 @@
+import time
+
+import numpy as np
+
+from feature_extractors import skipgram_analyzer
+from sklearn.feature_extraction import TfidfVectorizer, NMF
+from src.conf import RAW_DATA_FILES, TFIDF_FEATURES, SKIPGRAM_LIST,\
+    INTERIM_DATA_DIR, N_NMF_COMPONENTS, PROCESSED_DATA_DIR
+
+
+def main():
+    for f_name in RAW_DATA_FILES:
+        # tfidf
+        all_questions = np.load(INTERIM_DATA_DIR + 'q_' + f_name + '.npy')
+        nrows = len(all_questions) / 2
+        assert nrows == int(nrows)
+        nrows = int(nrows)
+
+        t = TfidfVectorizer(
+            max_df=0.95,
+            min_df=2,
+            stop_words='english',
+            max_features=TFIDF_FEATURES,
+            analyzer=lambda s:
+                skipgram_analyzer(s, skipgram_list=SKIPGRAM_LIST)
+            )
+
+        print('VECTORIZING', f_name, '...')
+        t0 = time.clock()
+        tfidf = t.fit_transform(all_questions)
+        print('Time:', time.clock() - t0)
+
+        nmf_tfidf = NMF(n_components=N_NMF_COMPONENTS,
+                        init='nndsvda')
+        print('NMF tfidf', f_name, '...')
+        t0 = time.clock()
+        W = nmf_tfidf.fit_transform(tfidf)
+        print('Time:', time.clock() - t0)
+
+        W = np.abs(W[:nrows, :] - W[nrows:, :])
+
+        X = np.load(INTERIM_DATA_DIR + 'X_' + f_name + '.npy')
+        X = np.hstack((X, W))
+        X.dump(PROCESSED_DATA_DIR + 'X_' + f_name + '.npy')
+    return
+
+
+if __name__ == '__main__':
+    main()
